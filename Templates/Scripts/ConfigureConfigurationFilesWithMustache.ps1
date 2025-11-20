72d6887ee7a829412d014eeafeaf5537d4ed180d
Sander Obdeijn <s.obdeijn@docsys.nl>
2025-11-20 22:32:48 +0100

    test

--- Templates/Scripts/ConfigureConfigurationFilesWithMustache.ps1
+++ Templates/Scripts/ConfigureConfigurationFilesWithMustache.ps1
@@ -1,7 +1,7 @@
 param ($mustacheConfigurationFilePath)
 Write-Host "Test"
 ls $PSScriptRoot
-
+Test-Path -Path $PSScriptRoot\Libraries\PSMustache-1.3.2\PSMustache.psd1
 Import-Module $PSScriptRoot\Libraries\PSMustache-1.3.2\PSMustache.psd1
 if (Test-Path -Path $mustacheConfigurationFilePath) {
     Write-Host "Configuration file found."
