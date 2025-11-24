754e6286722250005c6e1888c541982391ecc4e7
Sander Obdeijn <s.obdeijn@docsys.nl>
2025-11-24 13:32:52 +0100

    Use mustache for configuration (#3)

---
+++ Templates/Scripts/ConfigureConfigurationFilesWithMustache.ps1
@@ -0,0 +1,13 @@
+param ($mustacheConfigurationFilePath)
+# Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+Install-Module -Name PSMustache -Scope CurrentUser -Force
+if (Test-Path -Path $mustacheConfigurationFilePath) {
+    Write-Host "Configuration file found."
+    $values = Get-Content $mustacheConfigurationFilePath | ConvertFrom-Json
+
+    Get-ChildItem -Recurse -Include *.mustache -Name | ForEach-Object {
+        $configuredFile = $_.Replace('.mustache','') 
+        $template = Get-Content $_ | Out-String; ConvertFrom-MustacheTemplate -Template $template -Values $values | Out-File -FilePath $configuredFile -Encoding utf8
+        Write-Output "Configurationfile $configuredFile created."
+    }
+}
