b74a67215a3333baec6ddafae25b8834f8bfd6d3
Sander Obdeijn <s.obdeijn@docsys.nl>
2025-12-14 15:28:59 +0100

    test

--- Templates/Scripts/ConfigureConfigurationFilesWithMustache.ps1
+++ Templates/Scripts/ConfigureConfigurationFilesWithMustache.ps1
@@ -1,26 +1,59 @@
 param ($docSysConfigurationFilePath)
 Install-Module -Name PSMustache -Scope CurrentUser -Force
-Write-Output "Connection $dsoDatabaseConnection"
-if (-not (Test-Path -Path $docSysConfigurationFilePath)) {
-    Write-Output -ForegroundColor Red "DocSys Configuration file not found."
-    Exit
+
+function Format-ConfigurationFiles {
+    param ($docSysConfigurationFilePath)
+
+    if (-not (Test-Path -Path $docSysConfigurationFilePath)) {
+        Write-Output -ForegroundColor Red "DocSys Configuration file not found."
+        Exit
+    }
+
+    $values = Get-Content $docSysConfigurationFilePath -Raw | ConvertFrom-Json -AsHashtable
+    $secrets = $args
+
+    foreach ($secret in $secrets.GetEnumerator()) {
+        $splitSecret = $secret -split '='
+        Add-DottedPath -Root $values -Path $splitSecret[0] -Value $splitSecret[1]
+    }
+
+    Write-Output "Combined configuration input"
+    Write-Host ($values | ConvertTo-JSON -Depth 100)
+
+    # Get-ChildItem -Recurse -Include *.mustache -Name | ForEach-Object {
+    #     $configuredFile = $_.Replace('.mustache','') 
+    #     $template = Get-Content $_ | Out-String; ConvertFrom-MustacheTemplate -Template $template -Values $values | Out-File -FilePath $configuredFile -Encoding utf8
+    #     Write-Output "Configurationfile $configuredFile created."
+    # }
 }
 
-$values = Get-Content $docSysConfigurationFilePath | ConvertFrom-Json
-$secrets = $args
-foreach ($secret in $secrets) {
-    $splitSecret = $secret -split '='
-    $secretValue = $splitSecret[1]
+function Add-DottedPath {
+    param (
+        [hashtable]$Root,
+        [string]$Path,
+        [object]$Value
+    )
 
-    $values.DSO.DatabaseConnectionString = $secretValue
-    Write-Output $secret
+    $parts = $Path -split '\.'
+    $current = $Root
+
+    for ($i = 0; $i -lt $parts.Length; $i++) {
+        $key = $parts[$i]
+
+        if ($i -eq $parts.Length - 1) {
+            # Final element
+            $current[$key] = $Value
+        } else {
+            if (-not $current.ContainsKey($key)) {
+                $current[$key] = @{}
+            }
+            elseif (-not ($current[$key] -is [hashtable])) {
+                throw "Path conflict at '$key' in '$Path'"
+            }
+
+            $current = $current[$key]
+        }
+    }
 }
 
-Write-Output "Combined configuration input"
-Write-Host ($values | ConvertTo-JSON)
-
-Get-ChildItem -Recurse -Include *.mustache -Name | ForEach-Object {
-    $configuredFile = $_.Replace('.mustache','') 
-    $template = Get-Content $_ | Out-String; ConvertFrom-MustacheTemplate -Template $template -Values $values | Out-File -FilePath $configuredFile -Encoding utf8
-    Write-Output "Configurationfile $configuredFile created."
-}
+Format-ConfigurationFiles -docSysConfigurationFilePath $docSysConfigurationFilePath $args
