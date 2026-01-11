bb432c76500fbfacc68e5eec4cf663219dff243c
Sander Obdeijn <s.obdeijn@docsys.nl>
2026-01-11 16:03:22 +0100

    test

--- Templates/Scripts/DownloadDeployPackages.ps1
+++ Templates/Scripts/DownloadDeployPackages.ps1
@@ -8,20 +8,20 @@ $configuration = Get-Content $docSysConfigurationFilePath -Raw | ConvertFrom-Jso
 $configuration.PSObject.Properties | ForEach-Object {
     if ($_.Name -ne 'General')
     {
-        $name = $_.Name
+        $componentName = $_.Name
         $number = $_.Value.Version.Number
         
         if([bool]($_.Value.Version.PSobject.Properties.name -match "Client")) {
             $client = $_.Value.Version.Client
-            $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/$client-$name/$number/$name.zip?$deployPackageAccessToken"
+            $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/$client-$componentName/$number/$componentName.zip?$deployPackageAccessToken"
         }
         else {
-            $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/$name/$number/$name.zip?$deployPackageAccessToken"
+            $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/$componentName/$number/$componentName.zip?$deployPackageAccessToken"
         }
-        Invoke-WebRequest -Uri $downloadUri -OutFile "$name.zip"
-        Expand-Archive -Path "$name.zip" -DestinationPath "$deployPackageDirectory/$name"
+        Invoke-WebRequest -Uri $downloadUri -OutFile "$componentName.zip"
+        Expand-Archive -Path "$componentName.zip" -DestinationPath "$deployPackageDirectory/$componentName"
 
-        Write-Host "Downloaded $name version: $number for client: $client"
+        Write-Host "Downloaded $componentName version: $number for client: $client"
 
         if([bool]($_.Value.PSobject.Properties.name -match "Plugins"))
         {
@@ -31,20 +31,24 @@ $configuration.PSObject.Properties | ForEach-Object {
                     New-Item -path $deployPackageDirectory/Plugins -ItemType Directory
                 }
 
-                $name = $plugin.Name
+                if (-not(Test-Path $deployPackageDirectory/Plugins/$componentName -PathType Container)) {
+                    New-Item -path $deployPackageDirectory/Plugins/$componentName -ItemType Directory
+                }
+
+                $pluginName = $plugin.Name
                 $number = $plugin.Version.Number
                 $client = $null
 
                 if([bool]($plugin.Version.PSobject.Properties.name -match "Client")) {
                     $client = $_.Value.Version.Client
-                    $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/Plugins/$client-$name/$number/$name.dll?$deployPackageAccessToken"
+                    $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/Plugins/$client-$pluginName/$number/$pluginName.dll?$deployPackageAccessToken"
                 }
                 else {
-                    $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/Plugins/$name/$number/$name.dll?$deployPackageAccessToken"
+                    $downloadUri = "https://docsysdeploysg.blob.core.windows.net/deployment-container/Plugins/$pluginName/$number/$pluginName.dll?$deployPackageAccessToken"
                 }
-                Invoke-WebRequest -Uri $downloadUri -OutFile "$deployPackageDirectory/Plugins/$name.dll"
+                Invoke-WebRequest -Uri $downloadUri -OutFile "$deployPackageDirectory/Plugins/$componentName/$pluginName.dll"
 
-                Write-Host "Downloaded $name version: $number for client: $client"
+                Write-Host "Downloaded $pluginName version: $number for client: $client"
             }
         }
     }
