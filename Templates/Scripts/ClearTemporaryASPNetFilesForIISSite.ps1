d194610a0b40c1add65563e838c9264298cb406d
Sander Obdeijn <s.obdeijn@docsys.nl>
2026-01-12 15:14:18 +0100

    test

--- Templates/Scripts/ClearTemporaryASPNetFilesForIISSite.ps1
+++ Templates/Scripts/ClearTemporaryASPNetFilesForIISSite.ps1
@@ -2,5 +2,5 @@ param ($iisSiteName)
 $temporaryASPNetFilesFolder = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\${iisSiteName}"
 Write-Host "Clearing Temporary ASP.NET Files in directory: $temporaryASPNetFilesFolder"
 if (Test-Path -Path $temporaryASPNetFilesFolder) {
-    Get-ChildItem -Path $temporaryASPNetFilesFolder -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
+    Remove-Item -Path $temporaryASPNetFilesFolder -Recurse
 }
