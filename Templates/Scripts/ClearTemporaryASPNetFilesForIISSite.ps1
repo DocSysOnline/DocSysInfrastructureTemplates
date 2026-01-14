bcde682e9c772516de2780ee141284872e65b1b2
Sander Obdeijn <s.obdeijn@docsys.nl>
2026-01-14 16:51:39 +0100

    Retry temporary file cleanup (#15)

--- Templates/Scripts/ClearTemporaryASPNetFilesForIISSite.ps1
+++ Templates/Scripts/ClearTemporaryASPNetFilesForIISSite.ps1
@@ -1,6 +1,10 @@
 param ($iisSiteName)
 $temporaryASPNetFilesFolder = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\${iisSiteName}"
 Write-Host "Clearing Temporary ASP.NET Files in directory: $temporaryASPNetFilesFolder"
-if (Test-Path -Path $temporaryASPNetFilesFolder) {
+
+for ($i = 0; $i -lt 3; $i++) {
+    if (Test-Path -Path $temporaryASPNetFilesFolder -eq false) {
+        break
+    }
     Remove-Item -Path $temporaryASPNetFilesFolder -Recurse
 }
