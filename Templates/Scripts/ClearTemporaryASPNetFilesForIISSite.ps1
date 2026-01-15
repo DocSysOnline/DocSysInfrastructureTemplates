aad2830b710cc8e7b42104ffeca116414ff7e4e7
Sander Obdeijn <s.obdeijn@docsys.nl>
2026-01-15 07:35:23 +0100

    Enable multiple servers

--- Templates/Scripts/ClearTemporaryASPNetFilesForIISSite.ps1
+++ Templates/Scripts/ClearTemporaryASPNetFilesForIISSite.ps1
@@ -2,13 +2,14 @@ param ($iisSiteName)
 $temporaryASPNetFilesFolder = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\${iisSiteName}"
 Write-Host "Clearing Temporary ASP.NET Files in directory: $temporaryASPNetFilesFolder"
 
-for ($i = 0; $i -lt 3; $i++) {
+for ($i = 1; $i -le 3; $i++) {
     if ((Test-Path -Path $temporaryASPNetFilesFolder) -eq $false) {
         break
     }
     try
     {
         Remove-Item -Path $temporaryASPNetFilesFolder -Recurse
+        Write-Output "Clearing temporary files attempt: $i"
     }
     catch { }
 }
