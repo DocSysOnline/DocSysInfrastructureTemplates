ac3001ffdf0a172ac3e5b7f71f49a1fa9110a6b1
Sander Obdeijn <s.obdeijn@docsys.nl>
2026-05-05 22:26:36 +0200

    test

--- Templates/Scripts/CopySelectedPluginsToDeploymentStagingDirectory.ps1
+++ Templates/Scripts/CopySelectedPluginsToDeploymentStagingDirectory.ps1
@@ -31,7 +31,7 @@ if($null -ne $plugins)
 {
     New-Item "$component\bin\Plugins" -ItemType Directory
     foreach ($plugin in $plugins) {
-        Copy-Item -Path "Plugins\$component\$($plugin.Name)\*" -Destination "$component" -Recurse
+        Copy-Item -Path "Plugins\$component\$($plugin.Name)\*" -Destination "$component" -Recurse -Force
         Write-Host "Copied plugin $($plugin.Name) to component $component"
     }
 }
