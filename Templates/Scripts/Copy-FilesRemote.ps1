23dfff9c2fc8690f0520a009d493708aa1cf15ca
Sander Obdeijn <s.obdeijn@docsys.nl>
2026-08-12 16:09:31 +0200

    test

--- Templates/Scripts/Copy-FilesRemote.ps1
+++ Templates/Scripts/Copy-FilesRemote.ps1
@@ -13,7 +13,7 @@ if ($AccountType -eq 'UserAccount') {
     $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($username, $securepassword)
 }
 
-$files = Get-ChildItems -Path $source -Recurse
+$files = Get-ChildItem -Path $source -Recurse
 
 $sourcedirectory = Get-Item -Path $source
 $sourcepath = $sourcedirectory.FullName.TrimEnd('\')
